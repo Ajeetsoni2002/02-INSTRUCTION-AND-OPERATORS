5c14b94a57923c9a03dcdb16b512cc132c7e8f67
Ajeet Kumar Soni <94032343+Ajeetsoni2002@users.noreply.github.com>
2022-05-21 11:18:00 +0530

    Add files via upload

---
+++ CHAPTER 02 INSTRUCTION AND OPERATORS/oprater_precedence.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int x=3;
+int y=4;
+int z=5;
+ 
+ printf("value of 3*x + 4*y + 2*z is %d\n", 3*x + 4*y + 2*z);
+
+ printf("the value of 3*y/4*x IS %d\n",3*y/4*x);
+ 
+
+
+
+
+
+        return 0;
+
+}
